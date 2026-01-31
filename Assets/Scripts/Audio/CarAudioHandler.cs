8883cb4cfd7a2f89bcaa4d93fa8ea2543672a8f5
Oskari Rautala <oskari.rautala@dc.lab>
2026-01-31 22:24:20 +0200

    Added crash sounds and edited sound volumes a bit.

--- Assets/Scripts/Audio/CarAudioHandler.cs
+++ Assets/Scripts/Audio/CarAudioHandler.cs
@@ -9,6 +9,12 @@ public class CarAudioHandler : MonoBehaviour
     public AudioSource CarSoundSource;
     public AnimationCurve CarVolumeFromSpeed;
 
+    public AudioSource CrashSource;
+    public float CrashPitchMaxOffset;
+    public float MinTimeBetweenCrashSounds = 0.5f;
+
+    private float lastCrashSound = 0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -27,6 +33,35 @@ public class CarAudioHandler : MonoBehaviour
         WindSoundSource.volume = WindVolumeFromSpeed.Evaluate(speedLerp);
 
         CarSoundSource.volume = CarVolumeFromSpeed.Evaluate(speedLerp);
+    }
+
+    public void PlayCrashingSound()
+    {
+        if (Time.timeSinceLevelLoad - lastCrashSound < MinTimeBetweenCrashSounds)
+        {
+            return;
+        }
+
+        if (CrashSource.isPlaying)
+        {
+            return;
+        }
+
+        Debug.Log("CRASH");
+        float pitch = 1f;
+
+        if (CrashPitchMaxOffset > 0f)
+        {
+            pitch += Random.Range(-CrashPitchMaxOffset, CrashPitchMaxOffset);
+        }
+
+        CrashSource.pitch = pitch;
+        CrashSource.Play();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        PlayCrashingSound();
     }
 
 }
